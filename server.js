261f9aec063e1869dcefad6eff214fbbbc6786b9
Kevin W <kevinwestnyc@gmail.com>
2021-09-17 14:54:26 -0400

    server returning data!

--- server.js
+++ server.js
@@ -1,90 +1,169 @@
-const express = require("express");
-const path = require("path");
-const PORT = 3000;
-
-const axios = require("axios");
+const express = require('express');
+const path = require('path')
+const http = require('http')
+const socketIo = require('socket.io')
+const PORT = process.env.PORT || 3000;
 const config = require("dotenv").config();
-const TOKEN = process.env.TWITTER_BEARER_TOKEN;
 
 const app = express();
+const server = http.createServer(app)
+const io = socketIo(server)
 
 app.use(express.static(path.join(__dirname, "client", "build")));
 
 app.listen(PORT, () => console.log(`Server is listening on port: ${PORT}`));
 
-const rulesURL = "https://api.twitter.com/2/tweets/search/stream/rules";
-const streamURL =
-  "https://api.twitter.com/2/tweets/search/stream?tweet.fields=public_metrics&expansions=author_id";
-//&attachments.media_keys
+// Open a realtime stream of Tweets, filtered according to rules
+// https://developer.twitter.com/en/docs/twitter-api/tweets/filtered-stream/quick-start
 
-const rules = [{ value: "giveaway" }];
+const needle = require('needle');
 
-//get stream rules
-async function getRules() {
-  const response = await axios.get(rulesURL, {
-    headers: {
-      Authorization: `Bearer ${TOKEN}`,
-    },
-  });
-  console.log(response.body);
-  return response.body;
+// The code below sets the bearer token from your environment variables
+// To set environment variables on macOS or Linux, run the export command below from the terminal:
+// export BEARER_TOKEN='YOUR-TOKEN'
+const token = process.env.BEARER_TOKEN;
+
+const rulesURL = 'https://api.twitter.com/2/tweets/search/stream/rules';
+const streamURL = 'https://api.twitter.com/2/tweets/search/stream?tweet.fields=public_metrics&expansions=author_id&user.fields=profile_image_url,verified';
+//.fields=profile_image_url,verified
+
+// this sets up two rules - the value is the search terms to match on, and the tag is an identifier that
+// will be applied to the Tweets return to show which rule they matched
+// with a standard project with Basic Access, you can add up to 25 concurrent rules to your stream, and
+// each rule can be up to 512 characters long
+
+// Edit rules as desired below
+const rules = [{value: "coding"}];
+//(from:mattbarrows OR from:grantcohn OR from:49erswebzone)
+
+async function getAllRules() {
+
+    const response = await needle('get', rulesURL, {
+        headers: {
+            "authorization": `Bearer ${token}`
+        }
+    })
+
+    if (response.statusCode !== 200) {
+        console.log("Error:", response.statusMessage, response.statusCode)
+        throw new Error(response.body);
+    }
+    console.log(response.body)
+    return (response.body);
 }
 
-// set stream rules
+async function deleteAllRules(rules) {
+
+    if (!Array.isArray(rules.data)) {
+        return null;
+    }
+
+    const ids = rules.data.map(rule => rule.id);
+
+    const data = {
+        "delete": {
+            "ids": ids
+        }
+    }
+
+    const response = await needle('post', rulesURL, data, {
+        headers: {
+            "content-type": "application/json",
+            "authorization": `Bearer ${token}`
+        }
+    })
+
+    if (response.statusCode !== 200) {
+        throw new Error(response.body);
+    }
+
+    return (response.body);
+
+}
+
 async function setRules() {
-  const data = {
-    add: rules,
-  };
 
-  const response = await axios.post(rulesURL, data, {
-    headers: {
-      "content-type": "application/json",
-      Authorization: `Bearer ${TOKEN}`,
-    },
-  });
-  return response.body;
+    const data = {
+        "add": rules
+    }
+
+    const response = await needle('post', rulesURL, data, {
+        headers: {
+            "content-type": "application/json",
+            "authorization": `Bearer ${token}`
+        }
+    })
+
+    if (response.statusCode !== 201) {
+        throw new Error(response.body);
+    }
+
+    return (response.body);
+
 }
 
-// delete stream rules
-async function deleteRules(rules) {
-  if (!Array.isArray(rules.data)) {
-    return null;
-  }
+function streamConnect(retryAttempt) {
 
-  const ids = rules.data.map((rule) => rule.id);
+    const stream = needle.get(streamURL, {
+        headers: {
+            "User-Agent": "v2FilterStreamJS",
+            "Authorization": `Bearer ${token}`
+        },
+        timeout: 20000
+    });
 
-  const data = {
-    delete: {
-      ids: ids,
-    },
-  }
+    stream.on('data', data => {
+        try {
+            const json = JSON.parse(data);
+            console.log(json);
+            // A successful connection resets retry count.
+            retryAttempt = 0;
+        } catch (e) {
+            if (data.detail === "This stream is currently at the maximum allowed connection limit.") {
+                console.log(data.detail)
+                process.exit(1)
+            } else {
+                // Keep alive signal received. Do nothing.
+            }
+        }
+    }).on('err', error => {
+        if (error.code !== 'ECONNRESET') {
+            console.log(error.code);
+            process.exit(1);
+        } else {
+            // This reconnection logic will attempt to reconnect when a disconnection is detected.
+            // To avoid rate limits, this logic implements exponential backoff, so the wait time
+            // will increase if the client cannot reconnect to the stream. 
+            setTimeout(() => {
+                console.warn("A connection error occurred. Reconnecting...")
+                streamConnect(++retryAttempt);
+            }, 2 ** retryAttempt)
+        }
+    });
 
-  const response = await axios.post(rulesURL, data, {
-    headers: {
-      'content-type': 'application/json',
-      Authorization: `Bearer ${TOKEN}`,
-    },
-  });
-  console.log(response.body);
-  return response.body;
+    return stream;
+
 }
+
 
 (async () => {
-  let currentRules;
+    let currentRules;
 
-  try {
-    //get all stream rules
-    currentRules = await getRules();
+    try {
+        // Gets the complete list of rules currently applied to the stream
+        currentRules = await getAllRules();
 
-    //delete stream rules
-    await deleteRules(currentRules);
+        // Delete all rules. Comment the line below if you want to keep your existing rules.
+        await deleteAllRules(currentRules);
 
-    //set rules based on array above
-    await setRules();
-  } catch (error) {
-    console.error(error);
-    process.exit(1);
-  }
+        // Add rules to the stream. Comment the line below if you don't want to add new rules.
+        await setRules();
+
+    } catch (e) {
+        console.error(e);
+        process.exit(1);
+    }
+
+    // Listen to the stream.
+    streamConnect(0);
 })();
-
-
